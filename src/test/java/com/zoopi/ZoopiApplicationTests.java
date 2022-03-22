dad7520967b366656a666b4ffa6e80e7e7654ad8
seonpilKim <ksp970306@gmail.com>
2022-03-23 00:21:05 +0900

    init commit

---
+++ src/test/java/com/zoopi/ZoopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
