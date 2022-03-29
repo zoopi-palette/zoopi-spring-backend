5f7001e4395966cba572e548148e571149ac1939
seonpilKim <ksp970306@gmail.com>
2022-03-30 03:51:32 +0900

    Etc: 패키지 구조 변경
    멀티 모듈 구조로 구성
      - root project name: zoopi
      - module-api, module-common, module-service
    
    각 모듈별 의존성 추가 완료
    
    Resolve: #1

--- module-api/src/main/java/com/zoopi/ZoopiApplication.java
+++ module-api/src/main/java/com/zoopi/ZoopiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZoopiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZoopiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ZoopiApplication.class);
+    }
 }
