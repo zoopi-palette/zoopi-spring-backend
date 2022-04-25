4359ae015e55b65ee04dac0da347eb9c7ecf6896
seonpilKim <ksp970306@gmail.com>
2022-04-26 01:22:28 +0900

    Feat: 설정 파일 추가
    - QuerydslConfig, JpaAuditingConfig, SwaggerConfig
    
    Resolve: #3

---
+++ module-domain/src/main/java/com/zoopi/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.zoopi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
