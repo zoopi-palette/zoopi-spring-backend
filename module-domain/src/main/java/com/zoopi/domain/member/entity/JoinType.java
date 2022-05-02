53ad5cc020a9079115a265db08b50699b4a71da0
seonpilKim <ksp970306@gmail.com>
2022-05-03 08:04:12 +0900

    Feat: Member Entity 생성
    Resolve: #5

---
+++ module-domain/src/main/java/com/zoopi/domain/member/entity/JoinType.java
@@ -0,0 +1,5 @@
+package com.zoopi.domain.member.entity;
+
+public enum JoinType {
+	EMAIL, KAKAO, NAVER
+}
---
+++ module-domain/src/main/java/com/zoopi/domain/member/entity/Member.java
@@ -0,0 +1,67 @@
+package com.zoopi.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// TODO
+//  - 통신사 여부
+//  - 프로필 이미지
+
+@Entity
+@Getter
+@Table(name = "members")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	@Column(name = "member_email", unique = true)
+	private String email;
+
+	@Column(name = "member_password")
+	private String password;
+
+	@Column(name = "member_name")
+	private String name;
+
+	@Column(name = "member_phone", unique = true)
+	private String phone;
+
+	@CreatedDate
+	@Column(name = "member_create_date")
+	private LocalDateTime createdDate;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "member_join_type")
+	private JoinType joinType;
+
+	@Builder
+	public Member(String email, String password, String name, String phone, JoinType joinType) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.phone = phone;
+		this.joinType = joinType;
+	}
+}
