3202e03a5ce2a6f294a2d4ac4d3163a3341a087f
seonpilKim <ksp970306@gmail.com>
2022-05-05 03:25:44 +0900

    Style: 클래스 네이밍 변경
    - MemberController -> MemberAuthController
    - EmailValidationResponse -> ValidationResponse

--- module-api/src/main/java/com/zoopi/controller/member/response/ValidationResponse.java
+++ module-api/src/main/java/com/zoopi/controller/member/response/ValidationResponse.java
@@ -2,16 +2,14 @@ package com.zoopi.controller.member.response;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EmailValidationResponse {
+public class ValidationResponse {
 
-	private String email;
+	private String value;
 	private boolean isValidated;
 }
