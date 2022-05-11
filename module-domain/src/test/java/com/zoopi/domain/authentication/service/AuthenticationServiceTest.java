7e247cb8a244147b7608f693a79597702ceb5820
seonpilKim <ksp970306@gmail.com>
2022-05-11 19:24:07 +0900

    Test: AuthenticationService 테스트 코드 작성
    - Authentication의 createdDate -> 리플렉션 주입 코드 추가
    - validatePassword(), validateAuthenticationKey() 테스트 코드 작성
    
    Resolve: #9

--- module-domain/src/test/java/com/zoopi/domain/authentication/service/AuthenticationServiceTest.java
+++ module-domain/src/test/java/com/zoopi/domain/authentication/service/AuthenticationServiceTest.java
@@ -5,18 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.zoopi.domain.authentication.dto.response.AuthenticationResponse;
 import com.zoopi.domain.authentication.dto.response.AuthenticationResult;
 import com.zoopi.domain.authentication.entity.Authentication;
+import com.zoopi.domain.authentication.entity.AuthenticationStatus;
+import com.zoopi.domain.authentication.exception.PasswordMismatchException;
 import com.zoopi.domain.authentication.repository.AuthenticationRepository;
 import com.zoopi.infra.sms.SmsClient;
 import com.zoopi.util.AuthenticationCodeUtils;
@@ -34,7 +39,7 @@ class AuthenticationServiceTest {
 	private AuthenticationService authenticationService;
 
 	@Test
-	void sendAuthenticationCode_phone_sendSmsSuccess() throws Exception {
+	void sendAuthenticationCode_Phone_SendSmsSuccess() throws Exception {
 		// given
 		final String phone = "01012345678";
 		final String authenticationCode = AuthenticationCodeUtils.generateRandomAuthenticationCode(6);
@@ -48,12 +53,15 @@ class AuthenticationServiceTest {
 	}
 
 	@Test
-	void createAuthentication_codeAndPhone_success() throws Exception {
+	void createAuthentication_CodeAndPhone_Success() throws Exception {
 		// given
 		final String phone = "01012345678";
 		final String authenticationCode = AuthenticationCodeUtils.generateRandomAuthenticationCode(6);
+		final Authentication authentication = new Authentication(UUID.randomUUID().toString(), authenticationCode,
+			phone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now());
 		doReturn(Optional.empty()).when(authenticationRepository).findById(any(String.class));
-		doReturn(mock(Authentication.class)).when(authenticationRepository).save(any(Authentication.class));
+		doReturn(authentication).when(authenticationRepository).save(any(Authentication.class));
 
 		// when
 		final AuthenticationResponse authenticationResponse = authenticationService.createAuthentication(phone,
@@ -68,7 +76,7 @@ class AuthenticationServiceTest {
 		// given
 		final String phone = "01012345678";
 		doReturn(3).when(authenticationRepository)
-			.countByPhoneAndExpiredDateAfter(any(String.class), any(LocalDateTime.class));
+			.countByPhoneAndCreatedDateAfter(any(String.class), any(LocalDateTime.class));
 
 		// when
 		final int count = authenticationService.getCountOfAuthentication(phone);
@@ -78,7 +86,7 @@ class AuthenticationServiceTest {
 	}
 
 	@Test
-	void checkAuthenticationCode_nonexistentKey_expired() throws Exception {
+	void checkAuthenticationCode_NonexistentKey_Expired() throws Exception {
 		// given
 		final String authenticationKey = "2a6ec3d7-7147-4cdc-a6c5-fc5d937cfe76";
 		final String authenticationCode = "123456";
@@ -94,13 +102,13 @@ class AuthenticationServiceTest {
 	}
 
 	@Test
-	void checkAuthenticationCode_expiredCode_expired() throws Exception {
+	void checkAuthenticationCode_ExpiredCode_Expired() throws Exception {
 		// given
 		final String authenticationKey = "2a6ec3d7-7147-4cdc-a6c5-fc5d937cfe76";
 		final String authenticationCode = "123456";
 		final String phone = "01012345678";
-		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone,
-			LocalDateTime.now().minusMinutes(1));
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now().minusMinutes(5L));
 		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
 
 		// when
@@ -112,13 +120,13 @@ class AuthenticationServiceTest {
 	}
 
 	@Test
-	void checkAuthenticationCode_matchedCodeAndMyPhone_success() throws Exception {
+	void checkAuthenticationCode_MatchedCodeAndMyPhone_Success() throws Exception {
 		// given
 		final String authenticationKey = "2a6ec3d7-7147-4cdc-a6c5-fc5d937cfe76";
 		final String authenticationCode = "123456";
 		final String phone = "01012345678";
-		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone,
-			LocalDateTime.now().plusMinutes(5));
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now());
 		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
 
 		// when
@@ -126,18 +134,18 @@ class AuthenticationServiceTest {
 			authenticationCode, phone, authenticationKey);
 
 		// then
-		assertThat(result).isEqualTo(AuthenticationResult.SUCCESS);
+		assertThat(result).isEqualTo(AuthenticationResult.AUTHENTICATED);
 	}
 
 	@Test
-	void checkAuthenticationCode_mismatchedCode_mismatched() throws Exception {
+	void checkAuthenticationCode_MismatchedCode_Mismatched() throws Exception {
 		// given
 		final String authenticationKey = "2a6ec3d7-7147-4cdc-a6c5-fc5d937cfe76";
 		final String authenticationCode = "123456";
 		final String wrongAuthenticationCode = "634262";
 		final String phone = "01012345678";
-		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone,
-			LocalDateTime.now().plusMinutes(5));
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now());
 		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
 
 		// when
@@ -149,14 +157,14 @@ class AuthenticationServiceTest {
 	}
 
 	@Test
-	void checkAuthenticationCode_matchedCodeAndNotMyPhone_mismatched() throws Exception {
+	void checkAuthenticationCode_MatchedCodeAndNotMyPhone_Mismatched() throws Exception {
 		// given
 		final String authenticationKey = "2a6ec3d7-7147-4cdc-a6c5-fc5d937cfe76";
 		final String authenticationCode = "123456";
 		final String phone = "01012345678";
 		final String otherPhone = "01012341234";
-		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, otherPhone,
-			LocalDateTime.now().plusMinutes(5));
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, otherPhone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now());
 		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
 
 		// when
@@ -168,16 +176,128 @@ class AuthenticationServiceTest {
 	}
 
 	@Test
-	void deleteExpiredAuthenticationCodes_success() throws Exception {
+	void deleteExpiredAuthenticationCodes_Success() throws Exception {
 		// given
-		doReturn(List.of(mock(Authentication.class), mock(Authentication.class)))
-			.when(authenticationRepository).findAllByExpiredDateBefore(any(LocalDateTime.class));
-		doNothing().when(authenticationRepository).deleteAllInBatch(any(List.class));
+		doReturn(new ArrayList<>())
+			.when(authenticationRepository)
+			.findAllByStatusAndCreatedDateAfter(any(AuthenticationStatus.class), any(LocalDateTime.class));
 
 		// when
-		authenticationService.deleteExpiredAuthenticationCodes();
+		final boolean result = authenticationService.deleteExpiredAuthenticationCodes();
 
 		// then
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	void validatePassword_MatchedPassword_True() throws Exception {
+		// given
+		final String password = "qlalfqjsgh1!";
+		final String passwordCheck = "qlalfqjsgh1!";
+
+		// when
+		final boolean result = authenticationService.validatePassword(password, passwordCheck);
+
+		// then
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	void validatePassword_MismatchedPassword_ExceptionThrown() throws Exception {
+		// given
+		final String password = "qlalfqjsgh1!";
+		final String passwordCheck = "qlalfqjsgh1@";
+
+		// when
+		final Executable executable = () -> authenticationService.validatePassword(password, passwordCheck);
+
+		// then
+		assertThrows(PasswordMismatchException.class, executable);
+	}
+
+	@Test
+	void validateAuthenticationKey_NonExistentKey_Expired() throws Exception {
+		// given
+		final String phone = "01012341234";
+		final String authenticationKey = UUID.randomUUID().toString();
+		doReturn(Optional.empty()).when(authenticationRepository).findById(any(String.class));
+
+		// when
+		final AuthenticationResult result = authenticationService.validateAuthenticationKey(phone, authenticationKey);
+
+		// then
+		assertThat(result).isEqualTo(AuthenticationResult.EXPIRED);
+	}
+
+	@Test
+	void validateAuthenticationKey_ExpiredKey_Expired() throws Exception {
+		// given
+		final String phone = "01012341234";
+		final String authenticationKey = UUID.randomUUID().toString();
+		final String authenticationCode = "123456";
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now().minusMinutes(60L));
+		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
+
+		// when
+		final AuthenticationResult result = authenticationService.validateAuthenticationKey(phone, authenticationKey);
+
+		// then
+		assertThat(result).isEqualTo(AuthenticationResult.EXPIRED);
+	}
+
+	@Test
+	void validateAuthenticationKey_AuthenticatedKeyAndMyPhone_Authenticated() throws Exception {
+		// given
+		final String phone = "01012341234";
+		final String authenticationKey = UUID.randomUUID().toString();
+		final String authenticationCode = "123456";
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone);
+		authentication.authenticate();
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now().minusMinutes(10L));
+		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
+
+		// when
+		final AuthenticationResult result = authenticationService.validateAuthenticationKey(phone, authenticationKey);
+
+		// then
+		assertThat(result).isEqualTo(AuthenticationResult.AUTHENTICATED);
+	}
+
+	@Test
+	void validateAuthenticationKey_NotAuthenticatedKey_NotAuthenticated() throws Exception {
+		// given
+		final String phone = "01012341234";
+		final String authenticationKey = UUID.randomUUID().toString();
+		final String authenticationCode = "123456";
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, phone);
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now().minusMinutes(10L));
+		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
+
+		// when
+		final AuthenticationResult result = authenticationService.validateAuthenticationKey(phone, authenticationKey);
+
+		// then
+		assertThat(result).isEqualTo(AuthenticationResult.NOT_AUTHENTICATED);
+	}
+
+	@Test
+	void validateAuthenticationKey_NotMyPhone_NotAuthenticated() throws Exception {
+		// given
+		final String phone = "01012341234";
+		final String otherPhone = "01034341212";
+		final String authenticationKey = UUID.randomUUID().toString();
+		final String authenticationCode = "123456";
+		final Authentication authentication = new Authentication(authenticationKey, authenticationCode, otherPhone);
+		authentication.authenticate();
+		ReflectionTestUtils.setField(authentication, "createdDate", LocalDateTime.now().minusMinutes(10L));
+		doReturn(Optional.of(authentication)).when(authenticationRepository).findById(any(String.class));
+
+		// when
+		final AuthenticationResult result = authenticationService.validateAuthenticationKey(phone, authenticationKey);
+
+		// then
+		assertThat(result).isEqualTo(AuthenticationResult.NOT_AUTHENTICATED);
 	}
 
 }
