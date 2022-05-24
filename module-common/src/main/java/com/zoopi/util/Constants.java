36590b34aeb65234e1a3f1a270b76b3638ff13db
seonpilKim <ksp970306@gmail.com>
2022-05-25 02:47:07 +0900

    Feat: 상수 클래스 추가
    Resolve: #13

---
+++ module-common/src/main/java/com/zoopi/util/Constants.java
@@ -0,0 +1,13 @@
+package com.zoopi.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+	public static final String ALL_PATTERN = "/**";
+	public static final String EMPTY = "";
+	public static final String SPACE = " ";
+
+}
