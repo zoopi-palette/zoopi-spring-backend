792f07034386b6d2c13948f6a539249250fe7bfe
seonpilKim <ksp970306@gmail.com>
2022-07-03 09:37:36 +0900

    Refactor: ObjectMapper 빈으로 등록 & CustomAuthenticationEntryPoint 리팩토링
    See Also: #20

--- module-domain/src/main/java/com/zoopi/config/security/handler/CustomAuthenticationEntryPoint.java
+++ module-domain/src/main/java/com/zoopi/config/security/handler/CustomAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package com.zoopi.config.security.handler;
 
 import java.io.OutputStream;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,24 +13,22 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoopi.config.security.exception.CustomAuthenticationException;
-import com.zoopi.exception.response.ErrorCode;
 import com.zoopi.exception.response.ErrorResponse;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	private final ObjectMapper objectMapper;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException authException) {
 		final CustomAuthenticationException exception = (CustomAuthenticationException)authException;
-		final ErrorCode errorCode = exception.getErrorCode();
-		final int status = errorCode.getStatus();
-		final String code = errorCode.getCode();
-		final String message = errorCode.getMessage();
-		final List<ErrorResponse.FieldError> errors = exception.getErrors();
-		final ErrorResponse errorResponse = ErrorResponse.of(status, code, message, errors);
+		final ErrorResponse errorResponse = ErrorResponse.of(exception.getErrorCode(), exception.getErrors());
 
-		final ObjectMapper objectMapper = new ObjectMapper();
 		try (OutputStream os = response.getOutputStream()) {
 			objectMapper.writeValue(os, errorResponse);
 			os.flush();
