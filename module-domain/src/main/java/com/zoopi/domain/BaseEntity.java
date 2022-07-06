cd22024565ea8488fa6a81537fde4c35ebfe5632
seonpilKim <ksp970306@gmail.com>
2022-07-06 13:54:35 +0900

    Refactor: BaseEntity 추가
    대부분 Entity들이 공통으로 사용하는 createdAt, updatedAt을 MappedSuperclass으로 묶어서 상속 받는 방식으로 리팩토링

---
+++ module-domain/src/main/java/com/zoopi/domain/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.zoopi.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "create_at")
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "update_at")
+	private LocalDateTime updatedAt;
+
+}
