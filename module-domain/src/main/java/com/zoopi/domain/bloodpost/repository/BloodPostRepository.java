14c8be5758b720ca588d81eb902a193af38ab0f5
dongkyunkimdev <ddkds66@gmail.com>
2022-07-11 16:00:51 +0900

    feat: 헌혈 요청글 Application, Infrastructure Layer 구현
    - 프로토타입 개발이 목표이기 때문에 어느 정도의 결합도를 허용하여 구현
    - 추후 DIP, facade 패턴 등을 적용하여 레이어간 결합도를 최소화하도록 리팩토링할 예정
    - ex) Controller -> Facade -> Service <- ServiceImpl
    - ex) ServiceImpl -> Reader <- ReaderImpl -> Repository

---
+++ module-domain/src/main/java/com/zoopi/domain/bloodpost/repository/BloodPostRepository.java
@@ -0,0 +1,8 @@
+package com.zoopi.domain.bloodpost.repository;
+
+import com.zoopi.domain.bloodpost.entity.BloodPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BloodPostRepository extends JpaRepository<BloodPost, Long> {
+
+}
