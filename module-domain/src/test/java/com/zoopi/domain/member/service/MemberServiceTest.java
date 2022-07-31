b706866d610afa1ea958707a8ead8ac25369475d
seonpilKim <ksp970306@gmail.com>
2022-07-31 20:19:02 +0900

    Test: MemberService 단위테스트 코드 작성 및 MemberUtils 수정
    Resolve: #26

--- module-domain/src/test/java/com/zoopi/domain/member/service/MemberServiceTest.java
+++ module-domain/src/test/java/com/zoopi/domain/member/service/MemberServiceTest.java
@@ -1,27 +1,26 @@
 package com.zoopi.domain.member.service;
 
-import static com.zoopi.domain.member.util.MemberUtils.*;
+import static com.zoopi.exception.response.ErrorCode.*;
+import static com.zoopi.util.Constants.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.zoopi.domain.member.dto.SigninResponse;
-import com.zoopi.domain.member.entity.AuthorityType;
-import com.zoopi.domain.member.entity.JoinType;
 import com.zoopi.domain.member.entity.Member;
-import com.zoopi.domain.member.entity.MemberAuthority;
-import com.zoopi.domain.member.repository.MemberAuthorityRepository;
 import com.zoopi.domain.member.repository.MemberRepository;
+import com.zoopi.domain.member.util.MemberUtils;
+import com.zoopi.exception.EntityNotFoundException;
 import com.zoopi.util.JwtUtils;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,9 +28,6 @@ class MemberServiceTest {
 
 	@Mock
 	private MemberRepository memberRepository;
-
-	@Mock
-	private MemberAuthorityRepository memberAuthorityRepository;
 
 	@Mock
 	private JwtUtils jwtUtils;
@@ -43,56 +39,56 @@ class MemberServiceTest {
 	private MemberService memberService;
 
 	@Test
-	void validateEmail_UniqueEmail_True() throws Exception {
+	void isAvailableUsername_UniqueUsername_True() throws Exception {
 		// given
-		final String username = getMember(JoinType.EMAIL).getUsername();
+		final String username = MemberUtils.newMember().getUsername();
 
 		given(memberRepository.findByUsername(username)).willReturn(Optional.empty());
 
 		// when
-		final boolean isValidated = memberService.validateEmail(username);
+		final boolean isValidated = memberService.isAvailableUsername(username);
 
 		// then
 		assertThat(isValidated).isTrue();
 	}
 
 	@Test
-	void validateEmail_DuplicateEmail_False() throws Exception {
+	void isAvailableUsername_DuplicateUsername_False() throws Exception {
 		// given
-		final String username = getMember(JoinType.EMAIL).getUsername();
+		final String username = MemberUtils.newMember().getUsername();
 
 		given(memberRepository.findByUsername(username)).willReturn(Optional.of(mock(Member.class)));
 
 		// when
-		final boolean isValidated = memberService.validateEmail(username);
+		final boolean isValidated = memberService.isAvailableUsername(username);
 
 		// then
 		assertThat(isValidated).isFalse();
 	}
 
 	@Test
-	void validatePhone_UniquePhone_True() throws Exception {
+	void isAvailablePhone_UniquePhone_True() throws Exception {
 		// given
-		final String phone = getMember(JoinType.EMAIL).getPhone();
+		final String phone = MemberUtils.newMember().getPhone();
 
 		given(memberRepository.findByPhone(phone)).willReturn(Optional.empty());
 
 		// when
-		final boolean isValidated = memberService.validatePhone(phone);
+		final boolean isValidated = memberService.isAvailablePhone(phone);
 
 		// then
 		assertThat(isValidated).isTrue();
 	}
 
 	@Test
-	void validatePhone_DuplicatePhone_False() throws Exception {
+	void isAvailablePhone_DuplicatePhone_False() throws Exception {
 		// given
-		final String phone = getMember(JoinType.EMAIL).getPhone();
+		final String phone = MemberUtils.newMember().getPhone();
 
 		given(memberRepository.findByPhone(phone)).willReturn(Optional.of(mock(Member.class)));
 
 		// when
-		final boolean isValidated = memberService.validatePhone(phone);
+		final boolean isValidated = memberService.isAvailablePhone(phone);
 
 		// then
 		assertThat(isValidated).isFalse();
@@ -101,31 +97,29 @@ class MemberServiceTest {
 	@Test
 	void createMember_ValidArguments_SuccessToSave() throws Exception {
 		// given
-		final Member member = getMember(JoinType.EMAIL);
+		final Member member = MemberUtils.newMember();
 
 		given(memberRepository.save(any(Member.class))).willReturn(member);
 
 		// when
 		final Member savedMember = memberService.createMember(member.getUsername(), member.getPhone(), member.getName(),
-			member.getPassword(), JoinType.EMAIL);
+			member.getPassword(), member.getEmail());
 
 		// then
 		assertThat(member.getPhone()).isEqualTo(savedMember.getPhone());
 	}
 
 	@Test
-	void signin_ValidEmailAndMatchedPassword_Success() throws Exception {
+	void signin_ValidUsernameAndMatchedPassword_Success() throws Exception {
 		// given
-		final Member member = getMember(JoinType.EMAIL);
-		final List<MemberAuthority> authorities = getAuthorities(member, AuthorityType.ROLE_USER);
+		final Member member = MemberUtils.newMember();
 		final String accessToken = "accessToken";
 		final String refreshToken = "refreshToken";
 
 		given(memberRepository.findByUsername(any(String.class))).willReturn(Optional.of(member));
 		given(passwordEncoder.matches(any(String.class), any(String.class))).willReturn(true);
-		given(memberAuthorityRepository.findAllByMember(any(Member.class))).willReturn(authorities);
-		given(jwtUtils.generateJwt(member, authorities, JwtUtils.JwtType.ACCESS_TOKEN)).willReturn(accessToken);
-		given(jwtUtils.generateJwt(member, authorities, JwtUtils.JwtType.REFRESH_TOKEN)).willReturn(refreshToken);
+		given(jwtUtils.generateAccessToken(member)).willReturn(accessToken);
+		given(jwtUtils.generateRefreshToken(member)).willReturn(refreshToken);
 
 		// when
 		final SigninResponse response = memberService.signin(member.getUsername(), member.getPassword());
@@ -139,7 +133,7 @@ class MemberServiceTest {
 	@Test
 	void signin_MismatchedPassword_Fail() throws Exception {
 		// given
-		final Member member = getMember(JoinType.EMAIL);
+		final Member member = MemberUtils.newMember();
 		final String wrongPassword = "qlalfqjsgh2@";
 
 		given(memberRepository.findByUsername(any(String.class))).willReturn(Optional.of(member));
@@ -150,14 +144,14 @@ class MemberServiceTest {
 
 		// then
 		assertThat(response.getResult()).isEqualTo(SigninResponse.SigninResult.MISMATCHED_PASSWORD);
-		assertThat(response.getJwt().getAccessToken()).isEqualTo("");
-		assertThat(response.getJwt().getRefreshToken()).isEqualTo("");
+		assertThat(response.getJwt().getAccessToken()).isEqualTo(EMPTY);
+		assertThat(response.getJwt().getRefreshToken()).isEqualTo(EMPTY);
 	}
 
 	@Test
-	void signin_InvalidEmail_Fail() throws Exception {
+	void signin_InvalidUsername_Fail() throws Exception {
 		// given
-		final String wrongUsername = "nop@gmail.com";
+		final String wrongUsername = "nop";
 		final String wrongPassword = "qlalfqjsgh2@";
 
 		given(memberRepository.findByUsername(any(String.class))).willReturn(Optional.empty());
@@ -167,8 +161,67 @@ class MemberServiceTest {
 
 		// then
 		assertThat(response.getResult()).isEqualTo(SigninResponse.SigninResult.NONEXISTENT_USERNAME);
-		assertThat(response.getJwt().getAccessToken()).isEqualTo("");
-		assertThat(response.getJwt().getRefreshToken()).isEqualTo("");
+		assertThat(response.getJwt().getAccessToken()).isEqualTo(EMPTY);
+		assertThat(response.getJwt().getRefreshToken()).isEqualTo(EMPTY);
+	}
+
+	@Test
+	void getUsernameByPhone_ExistentPhone_Username() throws Exception {
+	    // given
+		final Member member = MemberUtils.newMember();
+
+		given(memberRepository.findByPhone(any(String.class))).willReturn(Optional.of(member));
+
+		// when
+		final String username = memberService.getUsernameByPhone(member.getPhone());
+
+		// then
+		assertThat(username).isEqualTo(member.getUsername());
+	}
+
+	@Test
+	void getUsernameByPhone_NonExistentPhone_ExceptionThrown() throws Exception {
+	    // given
+		final Member member = MemberUtils.newMember();
+
+		given(memberRepository.findByPhone(any(String.class))).willThrow(new EntityNotFoundException(MEMBER_NOT_FOUND));
+
+		// when
+		final Executable executable = () -> memberService.getUsernameByPhone(member.getPhone());
+
+		// then
+		assertThrows(EntityNotFoundException.class, executable);
+	}
+
+	@Test
+	void changePassword_ExistentUsername_Success() throws Exception {
+	    // given
+		final Member member = MemberUtils.newMember();
+		final String encodedPassword = "encodedPassword";
+
+		given(memberRepository.findByUsername(any(String.class))).willReturn(Optional.of(member));
+		given(passwordEncoder.encode(any(String.class))).willReturn(encodedPassword);
+
+	    // when
+		final boolean result = memberService.changePassword(member.getUsername(), member.getPassword());
+
+		// then
+		assertThat(result).isTrue();
+		assertThat(member.getPassword()).isEqualTo(encodedPassword);
+	}
+
+	@Test
+	void changePassword_NonExistentUsername_ExceptionThrown() throws Exception {
+		// given
+		final Member member = MemberUtils.newMember();
+
+		given(memberRepository.findByUsername(any(String.class))).willThrow(new EntityNotFoundException(MEMBER_NOT_FOUND));
+
+		// when
+		final Executable executable = () -> memberService.changePassword(member.getUsername(), member.getPassword());
+
+		// then
+		assertThrows(EntityNotFoundException.class, executable);
 	}
 
 }
--- module-domain/src/test/java/com/zoopi/domain/member/util/MemberUtils.java
+++ module-domain/src/test/java/com/zoopi/domain/member/util/MemberUtils.java
@@ -1,33 +1,17 @@
 package com.zoopi.domain.member.util;
 
-import java.util.List;
-
-import com.zoopi.domain.member.entity.AuthorityType;
-import com.zoopi.domain.member.entity.JoinType;
 import com.zoopi.domain.member.entity.Member;
-import com.zoopi.domain.member.entity.MemberAuthority;
 
 public class MemberUtils {
 
-	public static Member getMember(JoinType type) {
+	public static Member newMember() {
 		return Member.builder()
-			.joinType(type)
-			.phone("01012341234")
-			.name("홍길동")
+			.username("zoopi")
 			.password("qlalfqjsgh1!")
-			.username("zoopi@gmail.com")
+			.name("홍길동")
+			.email("zoopi@gmail.com")
+			.phone("01012341234")
 			.build();
-	}
-
-	public static List<MemberAuthority> getAuthorities(Member member, AuthorityType type) {
-		if (type.equals(AuthorityType.ROLE_USER)) {
-			return List.of(new MemberAuthority(member, AuthorityType.ROLE_USER));
-		} else if (type.equals(AuthorityType.ROLE_ADMIN)) {
-			return List.of(new MemberAuthority(member, AuthorityType.ROLE_USER),
-				new MemberAuthority(member, AuthorityType.ROLE_ADMIN));
-		} else {
-			return null;
-		}
 	}
 
 }
